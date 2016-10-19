45451c98cdc2e22bfe7e15b6850bdead416a278d
Steven M. Drucker <sdrucker@microsoft.com>
2016-10-18 18:03:21 -0700

    updated props

--- client/components/Index/Index.jsx
+++ client/components/Index/Index.jsx
@@ -1,51 +1,35 @@
 import React, { Component } from 'react';
 import * as D3 from "d3";
-import { SpringGrid, measureItems, makeResponsive } from 'react-stonecutter';
-class IndexComponent extends Component {
+import { CSSGrid, SpringGrid, measureItems, makeResponsive } from 'react-stonecutter';
+class IndexComponent extends Component {   
    componentDidMount() {
+     /*
       D3.json("http://localhost:3001/client/researchData.json", (error, data) => {
           this.setState({"researchData":data});
       });
+      */
   }
   render() {
-      if (this.state != null) {
-            if ("researchData" in this.state) {
-                var dataList =  this.state["researchData"].map( (val,i) => {return (<li className="grid-item" key={i}> <p className="research-item"> {i}: {val.caption}</p></li>);});
-                const Grid = makeResponsive(measureItems(SpringGrid), {maxWidth:1920});
-                return(<Grid
-                      component="ul"
-                      columns={5}
-                      columnWidth={100}
-                      gutterWidth={25}
-                      gutterHeight={5}                      
-                      springConfig={{ stiffness: 170, damping: 26 }}
-                    >
-                    {dataList}
-                    </Grid>);                                       
-            }
-        } else {
-            return (<div> nodata </div>);
-        }
-      }
-      /* 
     if (this.props.items.length === 0) {
       return (
-        <p ref="empty">Index is empty.</p>
+        <p ref="empty">Index is empty. </p>        
       );
     }
-
-    return (
-      <section>
-        <h2>react-webpack-boilerplate</h2>
-        <ul ref="indexList" className="index-list">
-          {this.props.items.map((item, index) => {
-            return (<li key={index}>item {item}</li>);
-          })}
-        </ul>
-      </section>
-    );
-    
-  }*/
+  var dataList =  this.props.items.map( (val,i) => {return (<li className="grid-item" key={i}> <p className="research-item"> {i}: {val.caption}</p></li>);});
+                
+  const Grid = makeResponsive(CSSGrid, {maxWidth: 1920});
+  return(<Grid
+        component="ul"
+        columns={6}
+        columnWidth={100}
+        gutterWidth={25}
+        gutterHeight={5}
+        duration={800}                      
+        springConfig={{ stiffness: 170, damping: 26 }}
+      >
+      {dataList}
+    </Grid>);
+  }    
 }
 
 IndexComponent.defaultProps = {
--- client/main.jsx
+++ client/main.jsx
@@ -6,10 +6,13 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import Index from 'components/Index/Index';
+import * as D3 from "d3";
+D3.json("http://localhost:3001/client/researchData.json", (error, data) => {
+    render(<div>
 
-
-render(<div>
-
-    <Index items={[1,2,3,4,5]} />
+    <Index items={data} />
     </div>,
  document.getElementById('js-main'));
+         
+});
+
