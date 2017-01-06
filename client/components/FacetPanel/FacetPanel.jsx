59cf69b42a13edd3477d32310bc9cedbb016fd74
Steven M. Drucker <sdrucker@microsoft.com>
2017-01-05 21:40:56 -0800

    sort by facetpanel, fixed TextureFromTouch collabs

--- client/components/FacetPanel/FacetPanel.jsx
+++ client/components/FacetPanel/FacetPanel.jsx
@@ -3,7 +3,11 @@ import {Table, Accordion, Panel} from 'react-bootstrap';
 import * as _ from "lodash";
 class FacetPanelComponent extends Component {
  componentWillMount() {
-      this.setState({"over":null});
+      this.setState(
+        {"over":null,
+         "order":"count",
+        "orderReverse":"desc"}
+    );
   };
   handleClick(title, val) {
       this.props.filter(title, val);
@@ -17,6 +21,14 @@ class FacetPanelComponent extends Component {
       this.props.clearFilter();
       this.setState({"over":null});
   };
+  setOrderBy(val) {
+      var doReverse = "asc";
+      if (this.state.order == val) {
+          if (this.state.orderReverse == "desc") doReverse = "asc";
+          else doReverse="desc";
+      }
+      this.setState({"order":val, "orderReverse":doReverse});
+  }
 
   render() {
     if (this.props.items.length === 0) {
@@ -29,8 +41,12 @@ class FacetPanelComponent extends Component {
     if (this.props.selected) {
         itemHoveredTags = this.props.selected.tags[panelSubject];
     }
-    var overItem = this.state.over;    
-    var mylist = _.orderBy(_.map(_.keys(this.props.items), (val) =>  {return {"name": val, "count": this.props.items[val]}}),"count","desc");
+    var overItem = this.state.over;
+    if (this.state.order == "count") {    
+        var mylist = _.orderBy(_.map(_.keys(this.props.items), (val) =>  {return {"name": val, "count": this.props.items[val]}}),"count", this.state.orderReverse);
+    } else {
+        var mylist = _.orderBy(_.map(_.keys(this.props.items), (val) =>  {return {"name": val, "count": this.props.items[val]}}),"name",this.state.orderReverse);    
+    }
     var dataList = [];
     var filterSpec = this.props.filterSpec;
 //    var dataList =  _.keys(this.props.items).map( (val,i) => {
@@ -55,8 +71,8 @@ class FacetPanelComponent extends Component {
         <thead>
         <tr>
             <th>   </th>
-            <th> {panelSubject} </th>
-            <th> count </th>
+            <th style={{"cursor":"pointer"}} onClick={(e)=>this.setOrderBy("name")}> {panelSubject} </th>
+            <th style={{"cursor":"pointer"}} onClick={(e)=>this.setOrderBy("count")}> count </th>
         </tr>
         </thead>
         <tbody>
